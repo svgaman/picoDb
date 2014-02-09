9db4c3b4485fc4fd4364b551ba3b2e7b748614f6
Frédéric Guillot <contact@fredericguillot.com>
2014-02-09 15:05:58 -0500

    Replace callable by Closure and transaction improvements

--- lib/PicoDb/Database.php
+++ lib/PicoDb/Database.php
@@ -30,7 +30,7 @@ class Database
     }
 
 
-    public static function bootstrap($name, callable $callback)
+    public static function bootstrap($name, \Closure $callback)
     {
         self::$instances[$name] = $callback;
     }
@@ -97,19 +97,25 @@ class Database
 
     public function startTransaction()
     {
-        $this->pdo->beginTransaction();
+        if (! $this->pdo->inTransaction()) {
+            $this->pdo->beginTransaction();
+        }
     }
 
 
     public function closeTransaction()
     {
-        $this->pdo->commit();
+        if ($this->pdo->inTransaction()) {
+            $this->pdo->commit();
+        }
     }
 
 
     public function cancelTransaction()
     {
-        $this->pdo->rollback();
+        if ($this->pdo->inTransaction()) {
+            $this->pdo->rollback();
+        }
     }
 
 
