1cd93e76ddc16761570eef72989afe5db43bb6d1
Frédéric Guillot <contact@fredericguillot.com>
2014-07-05 16:45:32 -0300

    Add Postgresql support

--- lib/PicoDb/Table.php
+++ lib/PicoDb/Table.php
@@ -4,6 +4,9 @@ namespace PicoDb;
 
 class Table
 {
+    const SORT_ASC = 'ASC';
+    const SORT_DESC = 'DESC';
+
     private $table_name = '';
     private $sql_limit = '';
     private $sql_offset = '';
@@ -67,8 +70,7 @@ class Table
 
         $result = $this->db->execute($sql, $values);
 
-        if ($result !== false && $result->rowCount() > 0) {
-
+        if ($result !== false/* && $result->rowCount() > 0*/) {
             return true;
         }
 
@@ -260,10 +262,10 @@ class Table
     }
 
 
-    public function orderBy($column, $order = 'ASC')
+    public function orderBy($column, $order = self::SORT_ASC)
     {
         $order = strtoupper($order);
-        $order = $order === 'ASC' || $order === 'DESC' ? $order : 'ASC';
+        $order = $order === self::SORT_ASC || $order === self::SORT_DESC ? $order : self::SORT_ASC;
 
         if ($this->sql_order === '') {
             $this->sql_order = ' ORDER BY '.$this->db->escapeIdentifier($column).' '.$order;
@@ -279,10 +281,10 @@ class Table
     public function asc($column)
     {
         if ($this->sql_order === '') {
-            $this->sql_order = ' ORDER BY '.$this->db->escapeIdentifier($column).' ASC';
+            $this->sql_order = ' ORDER BY '.$this->db->escapeIdentifier($column).' '.self::SORT_ASC;
         }
         else {
-            $this->sql_order .= ', '.$this->db->escapeIdentifier($column).' ASC';
+            $this->sql_order .= ', '.$this->db->escapeIdentifier($column).' '.self::SORT_ASC;
         }
 
         return $this;
@@ -292,10 +294,10 @@ class Table
     public function desc($column)
     {
         if ($this->sql_order === '') {
-            $this->sql_order = ' ORDER BY '.$this->db->escapeIdentifier($column).' DESC';
+            $this->sql_order = ' ORDER BY '.$this->db->escapeIdentifier($column).' '.self::SORT_DESC;
         }
         else {
-            $this->sql_order .= ', '.$this->db->escapeIdentifier($column).' DESC';
+            $this->sql_order .= ', '.$this->db->escapeIdentifier($column).' '.self::SORT_DESC;
         }
 
         return $this;
